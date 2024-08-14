0ee86eb00bdda316df04c56a52090b79ac282126
Dimitry <dimitrymmarinho@gmail.com>
2024-08-13 22:34:15 -0300

    Finalização Config Eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,25 +2,20 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginReact, { rules } from "eslint-plugin-react";
 
-
 export default [
   {
     files: ["**/*.{js,mjs,cjs,jsx}"],
-    rules:{
-
+    rules: {},
+    extends: {
+      plugin: react / jsx - runtime,
     },
-    extends:{
-      plugin:react/jsx-runtime,
-
-    }
   },
   {
     languageOptions: {
-       globals: globals.browser 
-      }
+      globals: globals.browser,
     },
+  },
 
   pluginJs.configs.recommended,
   pluginReact.configs.flat.recommended,
-  
 ];
