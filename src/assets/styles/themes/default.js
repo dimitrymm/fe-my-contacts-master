8e4f42e6f23b20977071d056398ff5cd37197850
Dimitry <dimitrymmarinho@gmail.com>
2024-08-15 10:03:02 -0300

    refactor: update components
    configurações inicias e criação dos Headers

---
+++ src/assets/styles/themes/default.js
@@ -0,0 +1,13 @@
+export default {
+    colors: {
+        background: "#444",
+
+        primary: {
+            text: "#faf5fc",
+            main: "#5061fc",
+            light: "#5061FC",
+            lighter: "#E0E3FF",
+            dark: "#3346F0",
+        },
+    },
+};
